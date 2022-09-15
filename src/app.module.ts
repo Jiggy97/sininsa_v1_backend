10852d2fbb41d6579b52e4129b70f0134561d4a9
Ahn <dkswnsdud159@naver.com>
2022-09-15 20:57:20 +0900

    design API structure - users & boards(...)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './users/users.module';
+import { BoardsModule } from './boards/boards.module';
+import { BoardsCommentsModule } from './boards/boards-comments/boards-comments.module';
+import { BoardsCommentsController } from './boards-comments/boards-comments.controller';
+import { BoardsCommentsRepliesModule } from './boards-comments-replies/boards-comments-replies.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UserModule, BoardsModule, BoardsCommentsModule, BoardsCommentsRepliesModule],
+  controllers: [AppController, BoardsCommentsController],
   providers: [AppService],
 })
 export class AppModule {}
