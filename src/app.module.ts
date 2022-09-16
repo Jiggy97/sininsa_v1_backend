ae181d2a8793cf99cea666265cc39847f15689c4
Ahn <dkswnsdud159@naver.com>
2022-09-16 17:37:31 +0900

    nestJS swagger install & import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './users/users.module';
 import { BoardsModule } from './boards/boards.module';
 import { BoardsCommentsModule } from './boards/boards-comments/boards-comments.module';
@@ -8,7 +6,6 @@ import { BoardsCommentsController } from './boards/boards-comments/boards-commen
 
 @Module({
   imports: [UserModule, BoardsModule, BoardsCommentsModule],
-  controllers: [AppController, BoardsCommentsController],
-  providers: [AppService],
+  controllers: [BoardsCommentsController],
 })
 export class AppModule {}
