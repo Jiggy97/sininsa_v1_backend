ee075c3cceb7006780a8267563ccfb296d34ee6c
Ahn <dkswnsdud159@naver.com>
2022-10-10 20:37:08 +0900

    register and login is being implemented

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,22 @@
 import { Crypto } from '@app/utils/crypto';
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserRequestDto } from './dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly usersService: UsersService) {}
 
-  async createUser({ email, password }: CreateUserDto) {
+  async registerUser({ email, password }: CreateUserRequestDto): Promise<void> {
+    try {
+      const hashedPassword = await Crypto.encrypt(password);
+      await this.usersService.create({
+        email,
+        password: hashedPassword,
+      });
+    }
     const hashedPassword = await Crypto.encrypt(password);
-    return this.usersService.createUser(email, hashedPassword);
+    return this.usersService.registerUser(email, hashedPassword);
   }
 
   async login(password: string, hash: string) {
