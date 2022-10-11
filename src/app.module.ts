6c505beeefbedcb4bbcfb80aa57feeade52b643a
Ahn <dkswnsdud159@naver.com>
2022-10-12 00:19:12 +0900

    error

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { AuthModule } from './authentication/auth/auth.module';
 import { PrismaModule } from '@app/prisma';
 import { AutenticationModule } from './authentication/authentication.module';
 import { TokenModule } from './authentication/token/token.module';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule } from '@app/jwt';
 import { AccessTokenService } from './authentication/token/access-token.service';
 import { RefreshTokenService } from './authentication/token/refresh-token.service';
 
